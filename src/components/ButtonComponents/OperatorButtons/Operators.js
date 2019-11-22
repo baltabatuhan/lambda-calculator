355171617d7064328890724a36fae8aa7ab3d22e
Batuhan Balta <batuhanbalta@batuhans-mbp.home>
2019-11-21 16:53:51 -0800

    mvp and stretch reached

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,14 @@
 import React from "react";
+import OperatorButton from "./OperatorButton";
 
-//import any components needed
-
-//Import your array data to from the provided data file
-
-const Operators = () => {
-  // STEP 2 - add the imported data to state
+const Operators = ({ operators, getValue }) => {
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="operators">
+      {operators.map((item, index) => (
+        <OperatorButton key={index} value={item} getValue={getValue} />
+      ))}
     </div>
   );
 };
+
+export default Operators;
